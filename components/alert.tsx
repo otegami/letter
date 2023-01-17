c5fef684a32d6332fe5b0c5565855f4a966c5633
otegami <a.s.takuya1026@gmail.com>
2023-01-17 19:23:39 +0900

    Removed unnecessary constant

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,6 +1,5 @@
 import Container from './container'
 import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
 
 type Props = {
   preview?: boolean
@@ -15,13 +14,13 @@ const Alert = ({ preview }: Props) => {
       })}
     >
       <Container>
-        <div className="py-2 text-center text-sm">
+        <div className="py-2 text-sm text-center">
           {preview ? (
             <>
               This page is a preview.{' '}
               <a
                 href="/api/exit-preview"
-                className="underline hover:text-teal-300 duration-200 transition-colors"
+                className="underline transition-colors duration-200 hover:text-teal-300"
               >
                 Click here
               </a>{' '}
@@ -31,8 +30,8 @@ const Alert = ({ preview }: Props) => {
             <>
               The source code for this blog is{' '}
               <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-blue-600 duration-200 transition-colors"
+                href={`https://github.com/otegami/letter`}
+                className="underline transition-colors duration-200 hover:text-blue-600"
               >
                 available on GitHub
               </a>
